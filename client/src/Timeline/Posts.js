6069aac9d7d963d43eb101ae566085e970e27ec4
tmachine <tmachine@sys08.PEC.IN>
2024-01-23 11:18:44 +0530

    main section

--- client/src/Timeline/Posts.js
+++ client/src/Timeline/Posts.js
@@ -1,8 +1,60 @@
 import React from 'react'
 
-function Posts() {
+import {Avatar} from '@mui/material'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import CommentIcon from '@mui/icons-material/Comment';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+
+
+import './Posts.css'
+
+function Posts({username,image,likes,timeAt}) {
   return (
-    <div>Posts</div>
+    <div className='single-post'>
+
+      <div className='post-header'>
+        
+        <div className='post-header-author'>
+           <Avatar>R</Avatar>
+            {username}
+            <span> . {timeAt}h</span>
+        </div>
+
+        <MoreHorizIcon/>
+      </div>
+
+      <div className='post-image'>
+        <img src={image} alt=''/>
+
+      </div>
+
+
+      <div className='post-footer'>
+
+        <div className='post-footer-icons'>
+
+          <div className='post-footer-icon-main'>
+            <FavoriteBorderIcon className='footer-icon'/>
+            <CommentIcon className='footer-icon'/>
+            <TelegramIcon className='footer-icon'/>
+          </div>
+
+
+          <div className='post-footer-icon-save'>
+            <BookmarkIcon/>
+          </div>
+
+         
+        </div>
+
+        Liked by {likes} people
+
+
+      </div>
+
+    </div>
   )
 }
 
--- client/src/Timeline/Timeline.js
+++ client/src/Timeline/Timeline.js
@@ -1,16 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Posts from './Posts'
 import Suggestion from './Suggestion'
 
 import './Timeline.css'
 
 function Timeline() {
+
+  const[post,setPost]=useState([
+
+    {
+      username:'ismartsanjoy',
+      image:"https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?auto=compress&cs=tinysrgb&w=600",
+      likes:'12',
+      timeAt:'3'
+    },
+    {
+      username:'sanju',
+      image:"https://images.pexels.com/photos/414144/pexels-photo-414144.jpeg?auto=compress&cs=tinysrgb&w=600",
+      likes:'34',
+      timeAt:'4'
+    },
+    {
+      username:'rahul',
+      image:"https://images.pexels.com/photos/1042423/pexels-photo-1042423.jpeg?auto=compress&cs=tinysrgb&w=600",
+      likes:'345',
+      timeAt:'23'
+    },
+    {
+      username:'ranjith',
+      image:"https://images.pexels.com/photos/1413412/pexels-photo-1413412.jpeg?auto=compress&cs=tinysrgb&w=600",
+      likes:'23',
+      timeAt:'6'
+    },
+    {
+      username:'sakthi',
+      image:"https://images.pexels.com/photos/440731/pexels-photo-440731.jpeg?auto=compress&cs=tinysrgb&w=600",
+      likes:'122',
+      timeAt:'5'
+    },
+
+
+  ]);
+
+
   return (
+
+
     <div className='main-timeline'>
 
+
+
+
+
       <div className='posts'>
-        <Posts/>
+
+       
+            
+       {
+        post.map((postinp)=>
+          <Posts 
+          username={postinp.username} 
+          image={postinp.image} 
+          likes={postinp.likes} 
+          timeAt={postinp.timeAt}
+          />
+        )
+       }
+      
+
+      
+
       </div>
+
+
+
+
+
+
+
 
       <div className='suggestion'>
         <Suggestion/>

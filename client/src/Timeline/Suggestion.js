5e281d56bd662a89c0138319c7612e8c6819d75e
tmachine <tmachine@sys08.PEC.IN>
2024-01-23 12:32:11 +0530

    all

--- client/src/Timeline/Suggestion.js
+++ client/src/Timeline/Suggestion.js
@@ -1,8 +1,74 @@
 import React from 'react'
 
+import {Avatar} from '@mui/material'
+
+import './Suggestion.css'
+
 function Suggestion() {
   return (
-    <div>Suggestion</div>
+    <div className='suggestion-main'>
+
+      <div className='suggestion-title'>Suggestion for you</div>
+
+      <div className='suggestion-divs'>
+
+        <div className='suggestion-div-single'>
+           <div className='suggestion-div-flex'>
+            <Avatar>R</Avatar>
+            <div>
+            <div>sam</div>
+            <div className='new-to-instagram-suggestion'>New to instagram</div>
+            </div>
+          </div>
+
+         <div> <button className='suggestion-btn'>Follow</button></div>
+        </div>
+
+
+
+        <div className='suggestion-div-single'>
+           <div className='suggestion-div-flex'>
+            <Avatar>R</Avatar>
+            <div>
+            <div>sam</div>
+            <div className='new-to-instagram-suggestion'>New to instagram</div>
+            </div>
+          </div>
+
+         <div> <button className='suggestion-btn'>Follow</button></div>
+        </div>
+
+
+        <div className='suggestion-div-single'>
+           <div className='suggestion-div-flex'>
+            <Avatar>R</Avatar>
+            <div>
+            <div>sam</div>
+            <div className='new-to-instagram-suggestion'>New to instagram</div>
+            </div>
+          </div>
+
+         <div> <button className='suggestion-btn'>Follow</button></div>
+        </div>
+
+
+        <div className='suggestion-div-single'>
+           <div className='suggestion-div-flex'>
+            <Avatar>R</Avatar>
+            <div>
+            <div>sam</div>
+            <div className='new-to-instagram-suggestion'>New to instagram</div>
+            </div>
+          </div>
+
+         <div> <button className='suggestion-btn'>Follow</button></div>
+        </div>
+
+      </div>
+
+
+
+    </div>
   )
 }
 
